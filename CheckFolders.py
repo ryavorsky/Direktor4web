80c9f2942e5a5f593b02490c79639cce9d54de86
ryavorsky <ryavorsky@gmail.com>
2014-08-22 17:29:58 +0400

    Revert "Parse input"
    This reverts commit cac4a2d61de223d34e744d057bd4ef06ca8a015f.

--- CheckFolders.py
+++ CheckFolders.py
@@ -18,7 +18,6 @@ def MakeSubfolder(inputFileName, outputDir) :
 
     # move the template files into the subfolder
     shutil.copy2('Report_template.html', subFolder)
-    shutil.copy2('key.txt', subFolder)
     shutil.copy2(inputFileName, subFolder + 'InputData'+ '_' + timestamp + '.txt')
     print('\nTemplate files copied to ' + subFolder + '\n')
 
