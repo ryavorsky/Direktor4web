13703332e2022c981e7f2e4b65ed638287b6f94f
ryavorsky <ryavorsky@gmail.com>
2014-08-24 11:27:33 +0400

    Templates folder added

--- Direktor4Web.py
+++ Direktor4Web.py
@@ -1,4 +1,7 @@
-import CheckFolders
+import time
+import os
+import shutil
+
 import ParseInput
 import StatValues
 import BuildHtml
@@ -8,15 +11,43 @@ def Main():
     inputFileName = "c:\\Direktor4web\\upload\\export_912.txt"
     outputDir = "c:\\Direktor4web\\reports\\"
 
-    [subFolder, rawData] = CheckFolders.MakeSubfolder(inputFileName, outputDir)
+    print 'Make subfolders. '
+    [subFolder, rawData] = MakeSubfolder(inputFileName, outputDir)
+  
+    print 'Parse input. '
     [graphData, statData] = ParseInput.DataFromFile(rawData, subFolder)
 
-    # Compute the statistics values and build the charts for the report
+    print 'Compute the statistics values and build the charts for the report. '
     StatValues.ComputeValues(subFolder, statData)
     BuildGraphs.BuildAllGraphs(subFolder, graphData)
+
+    print 'Create the final report. '
     BuildHtml.CreateReport(subFolder)
 
-    print('Hello World')
+
+def MakeSubfolder(inputFileName, outputDir) :
+    # Read the input file into the rawData variable
+    f = open(inputFileName, 'r')
+    rawData = f.readlines()
+    f.close()
+
+    # create subfolder for the report
+    t = time.localtime()
+    timestamp = str(t[0]) + '-' + str(t[1]) + '-' + str(t[2]) + '_' + str(t[3]) + 'h' + str(t[4]) + 'm' + str(t[5]) + 's'
+    subFolder = outputDir + 'Report_' + timestamp + '\\'
+    os.mkdir(subFolder)
+    print('\nThe output folder is created:\n\t' + subFolder + '\n')
+
+    # move the template files into the subfolder
+    sourceFolder = 'templates\\'
+    inputFiles = os.listdir(sourceFolder)
+    for fileName in inputFiles:
+        shutil.copy2(sourceFolder + fileName, subFolder)
+
+    shutil.copy2(inputFileName, subFolder + 'InputData'+ '_' + timestamp + '.txt')
+    print('\nTemplate files copied to ' + subFolder + '\n')
+
+    return [subFolder, rawData]
 
 
 Main()
