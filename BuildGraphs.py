f063c7306fd979a87aac01e039f2a2b72ad0fad1
ryavorsky <ryavorsky@gmail.com>
2014-09-04 20:30:53 +0400

    Пкфзр ыфьзду

--- BuildGraphs.py
+++ BuildGraphs.py
@@ -1,2 +1,10 @@
+import shutil
+import os
+
 def BuildAllGraphs(subFolder, graphData):
-    return
+    shutil.copy2(os.path.join(subFolder,'graph_sample.svg'), os.path.join(subFolder,'graph61.svg'))
+    shutil.copy2(os.path.join(subFolder,'graph_sample.svg'), os.path.join(subFolder,'graph62.svg'))
+    shutil.copy2(os.path.join(subFolder,'graph_sample.svg'), os.path.join(subFolder,'graph711.svg'))
+    shutil.copy2(os.path.join(subFolder,'graph_sample.svg'), os.path.join(subFolder,'graph712.svg'))
+    shutil.copy2(os.path.join(subFolder,'graph_sample.svg'), os.path.join(subFolder,'graph721.svg'))
+    shutil.copy2(os.path.join(subFolder,'graph_sample.svg'), os.path.join(subFolder,'graph722.svg'))
